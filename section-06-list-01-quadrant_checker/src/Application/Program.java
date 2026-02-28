543e7d92ed1b4d5e9e11998c6d0a272a74e486d8
Marcell Barreto <marcellbsouzas@gmail.com>
2026-02-28 19:19:45 -0300

    feat: section 06 - list 01 - exercise 02 - quadrant checker

---
+++ section-06-list-01-quadrant_checker/src/Application/Program.java
@@ -0,0 +1,32 @@
+package Application;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        while (x != 0 && y != 0) {
+            if (x > 0 && y > 0) {
+                System.out.println("First");
+            }
+            else if (x < 0 && y > 0) {
+                System.out.println("Second");
+            }
+            else if (x < 0 && y < 0) {
+                System.out.println("Third");
+            }
+            else {
+                System.out.println("Fourth");
+            }
+            x = sc.nextInt();
+            y = sc.nextInt();
+        }
+
+        sc.close();
+    }
+}
